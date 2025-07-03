7ce494c845bd9fc2e74827f9c863ceac6d8d5b95
kyomel <michaellapandio04@gmail.com>
2025-07-03 23:17:57 +0700

    feat: add collision detection between player and asteroids with game over state

--- circleshape.py
+++ circleshape.py
@@ -1,3 +1,4 @@
+from turtle import position
 import pygame
 
 # Base class for game objects
@@ -20,3 +21,6 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collides_with(self, other):
+        return self.position.distance_to(other.position) <= self.radius + other.radius
