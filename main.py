2b405e0e74be2497fbfb8f3aa03467565d32b8db
kyomel <michaellapandio04@gmail.com>
2025-07-03 22:29:57 +0700

    feat: add frame rate limiter and delta time tracking to game loop

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         for event in pygame.event.get():
@@ -15,6 +17,9 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+
+        # limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
